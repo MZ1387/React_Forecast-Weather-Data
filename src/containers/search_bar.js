1d920721ce605c2efd8e4512cf9433adeef5990d
Mario Zamora <maza1387@gmail.com>
2017-06-23 14:29:13 -0700

    Create SearchBar and WeatherList container, Create Weather reducer, Create Weather action

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
+  }
+
   render() {
     return (
       <div>
-        <form className='input-group'>
-          <input />
+        <form onSubmit={this.onFormSubmit} className='input-group'>
+          <input
+            placeholder='Search any city to see its 5 day forcast'
+            className='form-control'
+            value={this.state.term}
+            onChange={this.onInputChange}
+          />
           <span className='input-group-btn'>
             <button type='submit' className='btn btn-secondary'>Submit</button>
           </span>
@@ -14,3 +44,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
